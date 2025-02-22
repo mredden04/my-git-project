4791b3bedf2dadffeddeb6921f1c2ad1e1dafbf2
McKinley <redden.mckinley@gmail.com>
2025-02-22 00:32:30 +0000

    Added resume and projects pages

--- PersonalWebsite/app.py
+++ PersonalWebsite/app.py
@@ -18,3 +18,14 @@ def hello_world():
 def about_me():
     return render_template("about_me.html")
 
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+@app.route("/home")
+def home():
+    return render_template("index.html")
